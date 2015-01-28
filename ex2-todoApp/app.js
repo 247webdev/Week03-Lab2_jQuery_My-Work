62f870f90823998c677c48ce12e6dafc8ec5788e
Christian Chandler <247webdev+github@googlemail.com>
2015-01-27 20:02:08 -0800

    refactored js file

--- ex2-todoApp/app.js
+++ ex2-todoApp/app.js
@@ -1,45 +1,24 @@
 $(document).ready(function(){
 
-	$( "*" ).removeClass( "hilite" );
-
 	$('form').on('submit', function(e){
-
 		var toDoItem = $('input[type=text').val();
-
 		if( toDoItem.length === 0 ){
 			e.preventDefault();
 			return;
 		} else {
 			$('ul').append('<li><input type="checkbox">' + toDoItem + '<button type="button">X</button></li>');
 			$('#newToDo').focus().val("");
-
-//this line doesn't work by itself  $('ul').children().last().append( '<li><input type="checkbox">' + toDoItem + '</li>' );
-// if( $('ul').children().length === 0 ){
-// 	$('ul').append('<li><input type="checkbox">'+ toDoItem +'</li>');
-// } else {
-// 	$('ul').children().last().append( '<li><input type="checkbox">' + toDoItem + '</li>' );
-// }
 		}
-
-		// css('background', $('#newEvenColor').val() );
-
-
 	});
-//on adds, check if .html() === ""
-	//if "" use .html('<li>something</li>')
-	//else use $('ul').children().first().append('<li>something</li>')
 
-
-//clicking a todo item allows that todo to be struck thru   
-	$('body').on('click', 'input[type=checkbox]', function(e){  // 'body' is now a delegate and the click is looking for a click on a button 
-		// console.log(this);
+	$('body').on('click', 'input[type=checkbox]', function(e){
 		$(this).parent().toggleClass('strikeThrough');
-//			alert("hi!");
+		$(this).next().removeClass('strikeThrough');
 	});
-	$('body').on('click', 'button', function(e){  // 'body' is now a delegate and the click is looking for a click on a button 
+
+	$('body').on('click', 'button', function(e){
 		// console.log(this);
 		$(this).parent().remove();
 	});
-});
 
-//		console.log( "toDoItem " +toDoItem );
+});
