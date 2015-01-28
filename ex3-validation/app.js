de1071045c5dc247bb21fd04f44daeefbe203d50
Christian Chandler <247webdev+github@googlemail.com>
2015-01-28 09:32:08 -0800

    putting changes to ex2 and ex3

--- ex3-validation/app.js
+++ ex3-validation/app.js
@@ -1,23 +1,59 @@
 $(document).ready(function(){
-	$('#even').on('submit', function(e){
+	function showWarning(x){
+		$('#warningMessage').removeClass('off').html(x);	
+	}
+	function hideWarning(x){
+		$('#warningMessage').addClass('off').html(x);
+	}
 
-		if($('input[type=text').val().length === 0 ){
-			e.preventDefault();
-			return;
+	$('input[type=password]').keydown( function(e){
+		var passLength = $('input[type=password]').val().length;
+		if( passLength < 6 ){
+			$('#warningMessage').hasClass('off') ? showWarning(passLength) : void(0) ;
+		} else {
+			!$('#warningMessage').hasClass('off') ? void(0) : showWarning(passLength) ;
 		}
-
-		$('tbody tr:even').css('background', $('#newEvenColor').val() );
-
+		//showWarning()
 	});
-	$('#odd').on('submit', function(e){
 
-		if($('input[type=text').val().length === 0 ){
-			e.preventDefault();
-			return;
+	$('input[type=password]').submit( function(e){
+		if( true ){
+			console.log("inside submit");
 		}
-
-		$('tbody tr:odd').css('background', $('#newOddColor').val() );
-
 	});
+
+	//$(this).filter(".block-left").addClass('fadeInLeft');
+// $(this).filter(".block-right").addClass('fadeInRight');
+
+
+
+	// $();
+
+	// $('body').html("please enter a password");
+
+
+
+	// });
+
+	// 	if($('input[type=text').val().length === 0 ){
+	// 		e.preventDefault();
+	// 		return;
+	// 	}
+
+	// 	$('tbody tr:even').css('background', $('#newEvenColor').val() );
+
+
+
+
+	// $('#odd').on('submit', function(e){
+
+	// 	if($('input[type=text').val().length === 0 ){
+	// 		e.preventDefault();
+	// 		return;
+	// 	}
+
+	// 	$('tbody tr:odd').css('background', $('#newOddColor').val() );
+
+	// });
 
 });
